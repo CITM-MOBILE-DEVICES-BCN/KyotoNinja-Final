db3b7b7a2030bd9ebcedff2ebe58e7ce78041fe0
faelion <101017492+faelion@users.noreply.github.com>
2025-01-18 18:26:30 +0100

    old gameplay remove

--- KyotoNinja-Final/Assets/Scripts/Obstacles/InheritsFromObstaclesProvider/AirObstacleProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GroundObstacleProvider", menuName = "ObstacleProvider/Ground")]
-public class GroundObstacleProvider : BaseObstacleProvider
-{
-
-}
--- KyotoNinja-Final/Assets/Scripts/Obstacles/InheritsFromObstaclesProvider/GroundObstacleProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AirObstacleProvider", menuName = "ObstacleProvider/Air")]
-public class AirObstacleProvider : BaseObstacleProvider
-{
-
-}
